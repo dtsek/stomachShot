3e9efe99e254aa361fc7a02af4d2c3d6b06fb1cf
Dennis Tsekouras <dionysios.tsekouras@cern.ch>
2019-10-05 18:18:45 +0200

    Main code ready

--- wound.py
+++ wound.py
@@ -1,9 +1,25 @@
-import time
-import picamera
+#!/usr/bin/python3
 
-with picamera.PiCamera() as camera:
-	camera.resolution = (1920,1080)
-	camera.hflip = True
-	camera.vflip = True
+# Libraries
+from picamera import PiCamera
+from time import sleep
 
-	camera.start_preview()
+def holeView():
+
+  # create camera object
+  camera = PiCamera()
+  #camera.rotation = 180
+  #camera.resolution = (1920, 1080)
+  #camera.framerate = 30
+  #camera.brightness = 100
+  #cameera.contrast = 1
+
+  while True:
+    camera.start_preview()
+    sleep(100)  #bug avoidance
+    camera.stop_preview()
+
+if __name__ == '__main__':
+  print('Starting the program...\n')
+  holeView()
+
